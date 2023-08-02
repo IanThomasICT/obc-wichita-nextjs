88f89a5079daffda69dae7dcdc0d08273145a316
Ian Thomas <ianthomasict@gmail.com>
2023-08-02 00:55:10 -0500

    enhance: add transitions to hero text and improve padding on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import GoogleMapsIcon from "@/public/images/maps_icon.svg"
 import heroImage from "@/public/images/pastor-preaching-pulpit.jpg"
 import sanctuaryImage from "@/public/images/sanctuary-full-view.jpg"
+import { motion } from "framer-motion"
 import { ChevronDown, Clock10, MapPin, Video } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
@@ -12,13 +16,17 @@ import { Separator } from "@/components/ui/separator"
 import { Icons } from "@/components/Icons"
 import ScrollLink from "@/components/ScrollLink"
 
+type Props = {
+  className?: string
+}
+
 export default function IndexPage() {
   return (
     <>
       <HeroSection />
-      <CardSection />
+      <CardSection className="mx-6 my-16 flex flex-col justify-center gap-8 md:flex-row" />
       <section className="h-full bg-accent">
-        <p className="mx-auto max-w-5xl p-10 text-center text-2xl font-semibold text-foreground">
+        <p className="mx-auto max-w-5xl p-10 py-20 text-center text-2xl font-semibold leading-10 text-foreground">
           Whether you've grown up in church all your life or have never been to church before, we hope that Olivet will be a warm and welcoming place for you where you're engaged with the truth of
           God's Word and the love of God's people.
         </p>
@@ -31,8 +39,8 @@ export default function IndexPage() {
           <Image src={sanctuaryImage} alt="full-view of olivet sanctuary" className="blur-[2px]" />
           <div className="absolute inset-0 z-10 bg-foreground opacity-70"></div>
 
-          <div className="absolute inset-0 z-20 mx-auto flex max-w-screen-sm flex-col justify-center gap-6 text-background md:max-w-2xl xl:max-w-4xl">
-            <h2 className="text-center text-lg font-semibold sm:text-2xl md:text-4xl lg:text-5xl">Worship Gathering Information</h2>
+          <div className="absolute inset-0 z-20 mx-auto flex max-w-screen-sm flex-col justify-center gap-10 text-background md:max-w-2xl xl:max-w-4xl">
+            <h2 className="text-center text-lg font-semibold sm:text-2xl md:text-4xl lg:text-6xl">Worship Gathering Information</h2>
             <Separator />
 
             {/* Service / Location card */}
@@ -46,14 +54,14 @@ export default function IndexPage() {
                 </p>
                 <p className="flex flex-col gap-4 text-lg tracking-wider xl:text-2xl">
                   <span className="-ml-8 flex items-baseline gap-2 text-3xl font-bold leading-3 xl:text-4xl">
-                    <MapPin /> Location
+                    <Image src={GoogleMapsIcon} alt="Map pin" width={18} height={18} className="mr-1.5" /> Location
                   </span>
                   {siteConfig.address.street}
                   <br />
                   {siteConfig.address.city}, {siteConfig.address.state} {siteConfig.address.zip}
                 </p>
               </div>
-              <div className="text-muted-background grid flex-auto items-center gap-2 text-lg tracking-wider sm:text-xl xl:text-2xl">
+              <div className="text-muted-background grid flex-auto items-center gap-2 text-lg tracking-wider sm:text-xl xl:gap-10 xl:text-3xl">
                 <span>Main parking lot on the north side.</span>
                 <span>Additional parking is available on the west side of High St.</span>
               </div>
@@ -81,7 +89,7 @@ export default function IndexPage() {
                 {/* Location time */}
                 <p className="text-muted-background mb-2 flex flex-col gap-4 whitespace-nowrap text-xl tracking-wider">
                   <span className="-ml-8 flex items-baseline gap-2 text-3xl font-bold leading-3">
-                    <MapPin /> Location
+                    <Image src={GoogleMapsIcon} alt="Map pin" width={10} height={10} /> Location
                   </span>
                   {siteConfig.address.street}
                   <br />
@@ -101,19 +109,32 @@ export default function IndexPage() {
   )
 }
 
-function HeroSection(props: { className?: string }) {
+function HeroSection({ className }: Props) {
   return (
-    <section className={cn("relative h-full w-screen", props.className)}>
+    <section className={cn("relative h-full w-screen", className)}>
       <Image src={heroImage} alt={"pastor preaching in pulpit"} className="h-full w-full" />
       <div className="absolute top-0 h-full w-full pb-6 pl-24 pt-24 text-white">
         <div className="flex h-full flex-col items-start justify-end gap-6 ">
-          <h1 className="max-w-lg text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">Our church exists for the glory of God</h1>
-          <div className="flex max-w-[980px] flex-col items-start gap-2">
-            <p className="text-muted-background max-w-[700px] text-lg sm:text-xl">
-              We seek to glorify Him by <strong>loving Him</strong> with all of our being, <strong>loving others</strong> as ourselves, and <strong>making disciples</strong> of all ages and nations
-              starting right here in Wichita.
-            </p>
-            <p className="text-muted-background max-w-[700px] text-lg sm:text-xl">We invite you to join us for worship this Sunday at 10:00 a.m</p>
+          <motion.h1
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0, transition: { delay: 0.3, duration: 2 } }}
+            className="max-w-lg text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl"
+          >
+            Our church exists for the glory of God
+          </motion.h1>
+          <div className="flex max-w-[980px] flex-col items-start gap-6">
+            <motion.p
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0, transition: { delay: 0.6, duration: 2 } }}
+              className="text-muted-background max-w-[700px] text-lg tracking-wide sm:text-xl"
+            >
+              We seek to glorify Him by <strong className="mx-0.5 text-2xl text-muted-foreground brightness-150">loving Him</strong> with all of our being,{" "}
+              <strong className="mx-0.5 text-2xl text-muted-foreground brightness-150">loving others</strong> as ourselves, and{" "}
+              <strong className="mx-0.5 text-2xl text-muted-foreground brightness-150">making disciples</strong> of all ages and nations starting right here in Wichita.
+            </motion.p>
+            <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 2 } }} className="text-muted-background max-w-[700px] text-lg sm:text-xl">
+              We invite you to join us for worship this Sunday at 10:00 a.m
+            </motion.p>
           </div>
           <div className="flex flex-grow gap-4">
             <ScrollLink href="#worship-info" className={cn(buttonVariants({ variant: "secondary", size: "lg" }), "text-lg")}>
@@ -129,9 +150,9 @@ function HeroSection(props: { className?: string }) {
   )
 }
 
-function CardSection() {
+function CardSection({ className }: Props) {
   return (
-    <section className="m-6 flex flex-col justify-center gap-6 md:flex-row">
+    <section className={className}>
       <Card className="mx-auto w-full max-w-md flex-1 md:mx-0">
         <CardHeader>
           <CardTitle className="text-lg sm:text-xl lg:text-2xl">When We Gather</CardTitle>
@@ -194,17 +215,17 @@ function CardSection() {
         </CardHeader>
         <CardContent className="grid max-w-fit space-y-2">
           <Link href="/resources/sermon-videos">
-            <Button variant={"outline"} className="flex w-full justify-start gap-4 text-lg">
+            <Button variant={"outline"} className="flex w-full justify-start gap-4 py-6 pr-6 text-lg">
               <Video /> Watch Sermons
             </Button>
           </Link>
           <Link href="/resources/recommended-reading">
-            <Button variant={"outline"} className="flex w-full justify-start gap-4 text-lg">
+            <Button variant={"outline"} className="flex w-full justify-start gap-4 py-6 pr-6 text-lg">
               <Icons.bookMarked /> Recommended Reading
             </Button>
           </Link>
           <Link href={siteConfig.links.social.spotify}>
-            <Button variant={"outline"} className="flex w-full justify-start gap-4 text-lg">
+            <Button variant={"outline"} className="flex w-full justify-start gap-4 py-6 pr-6 text-lg">
               <Icons.spotify className="h-6 w-6" /> Recommended Music
             </Button>
           </Link>
